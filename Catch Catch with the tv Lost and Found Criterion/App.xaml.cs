5a40e92f45146cc3f4e9b7226a0a2fbff91b5055
Thomas Vargas <Thomas Vargas@THOMAS>
2026-04-28 11:21:59 +0800

    Add project files.

---
+++ Catch Catch with the tv Lost and Found Criterion/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace Catch_Catch_with_the_tv_Lost_and_Found_Criterion
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
